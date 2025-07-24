b0e0a6e2ec256e720e5dc3049532fd146d7c6589
Cyanchlor <nailahz41@gmail.com>
2025-07-24 20:22:29 +0700

    ruby test
    run Ruby for the first time

---
+++ ruby-on-rails/redclover.rb
@@ -0,0 +1,12 @@
+puts "Guten Tag!"
+
+name = "Red Clover"
+age = 21
+
+puts "Mein Codename ist #{name} und ich bin #{age} Jahre alt."
+puts "Ich liebe es, Code zu schreiben!"
+puts "Ich hoffe, du hast einen tollen Tag!"
+
+5.times do
+    puts "Mach weiter so!"
+end
